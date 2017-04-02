7df2ad4bb245a6c76389e46c3b55daca9330b066
Steve Drew <drewypnl@hotmail.com>
2017-04-02 13:19:15 +0100

    First Version

--- listViewTutorial/MainActivity.cs
+++ listViewTutorial/MainActivity.cs
@@ -1,18 +1,33 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System.Collections.Generic;
 
 namespace listViewTutorial
 {
     [Activity(Label = "listViewTutorial", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private List<string> mItems;
+        private ListView mListView;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
+
+            mListView = FindViewById<ListView>(Resource.Id.myListView);
+            mItems = new List<string>();
+
+            mItems.Add("Steve");
+            mItems.Add("Kate");
+            mItems.Add("Ellie");
+
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, mItems);
+
+            mListView.Adapter = adapter;
         }
     }
 }
