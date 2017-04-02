17f075ae86ab5e802563e9c954f42d3a49b5658e
Steve Drew <drewypnl@hotmail.com>
2017-04-02 17:53:29 +0100

    Add click events

--- listViewTutorial/MainActivity.cs
+++ listViewTutorial/MainActivity.cs
@@ -2,6 +2,7 @@
 using Android.Widget;
 using Android.OS;
 using System.Collections.Generic;
+using System;
 
 namespace listViewTutorial
 {
@@ -16,19 +17,32 @@ namespace listViewTutorial
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            SetContentView (Resource.Layout.Main);
+            SetContentView(Resource.Layout.Main);
 
             mListView = FindViewById<ListView>(Resource.Id.myListView);
             mItems = new List<Person>();
 
-            mItems.Add(new Person() { FirstName = "Steve", LastName = "Drew", Age = "42", Gender = "Male" } );
-            mItems.Add(new Person() { FirstName = "Kate", LastName = "Drew", Age = "53", Gender = "Female" } );
-            mItems.Add(new Person() { FirstName = "Ellie", LastName = "Blenkin", Age = "24", Gender = "Female" } );
-           
+            mItems.Add(new Person() { FirstName = "Steve", LastName = "Drew", Age = "42", Gender = "Male" });
+            mItems.Add(new Person() { FirstName = "Kate", LastName = "Drew", Age = "53", Gender = "Female" });
+            mItems.Add(new Person() { FirstName = "Ellie", LastName = "Blenkin", Age = "24", Gender = "Female" });
+
             MyListViewAdapter adapter = new MyListViewAdapter(this, mItems);
 
             mListView.Adapter = adapter;
+
+            mListView.ItemClick += MListView_ItemClick;
+            mListView.ItemLongClick += MListView_ItemLongClick;
+        }
+        private void MListView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            Console.WriteLine(mItems[e.Position].FirstName);
+        }
+
+        private void MListView_ItemLongClick(object sender, AdapterView.ItemLongClickEventArgs e)
+        {
+            Console.WriteLine(mItems[e.Position].LastName);
         }
     }
 }
+
 
